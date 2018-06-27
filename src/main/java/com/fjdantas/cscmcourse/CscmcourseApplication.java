04c3c3de6aaa874574369c7d105d680070e8a46c
Fábio Dantas <fabiojdantas@ymail.com>
2018-06-27 18:16:16 -0300

    Project cscmcourse created!

---
+++ src/main/java/com/fjdantas/cscmcourse/CscmcourseApplication.java
@@ -0,0 +1,12 @@
+package com.fjdantas.cscmcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscmcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscmcourseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fjdantas/cscmcourse/CscmcourseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fjdantas.cscmcourse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CscmcourseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
